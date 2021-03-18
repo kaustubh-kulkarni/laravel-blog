78100f32a1eb68e0be7f4e494c85b842871f927d
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-18 14:20:35 +0100

    Created first controller PostController using artisan

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,7 @@
 <?php
-
+// Namespace let us use same functions in any other files without problems (Differenciation of functions).
 namespace App\Http\Controllers;
-
+// use is used to import specific class or namespace
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
